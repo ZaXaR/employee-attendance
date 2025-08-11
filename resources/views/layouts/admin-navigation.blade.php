e0668d6b6dcdb037b8517f7438f8ff3e8e649501
ZaXaR <zaxar1990jem@gmail.com>
2025-08-12 00:09:06 +0300

    fix outline current page on nav

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     {{-- @if (Auth::user()->isAdmin()) --}}
-                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('admin.attendance.index')" :active="request()->routeIs('admin.attendance.*')">
